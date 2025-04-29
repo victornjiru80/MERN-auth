695a03b4f26a09871d9b4c03f0d7acc5fb9ed35e
Victor <87243454+victornjiru80@users.noreply.github.com>
2025-04-29 17:45:11 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,27 +21,51 @@ const Header = () => {
 
 
   return (
-    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
-        <img src={assets.header_img} alt="" className='w-46 h-46 rounded-full mb-6' />
+
+      <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+
+       {!isUserLoggedIn ? (
+            <img src={assets.header_img} alt="" className='w-46 h-46 rounded-full mb-6' />
+            ) : (
+              <p></p>
+            ) } 
+
         <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hello { userData ? userData.name : 'Everyone'}!
            <img className='w-8 aspect-square' src={assets.hand_wave} alt="" />
         </h1>
-        <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our Community!</h2>
 
-        {isUserLoggedIn ? (
-            <>
-                <p className='mb-8 max-w-md'>
-                  Welcome to our community! We're thrilled to have you join us and become a part of something special. Here, you'll find a supportive and engaging environment where ideas are shared, connections are made, and everyone is valued. Whether you're here to learn, contribute, or simply connect with like-minded individuals, know that you’re among friends. We can’t wait to see the unique perspective and energy you bring—welcome aboard!
-                </p>
-                 <p className='mb-8 max-w-md'>
-                    Now that you're here with us, We would like to show you what our community is about and hopefully your journey with us will be worthwhile!
-                 </p>
-            </>
+        {!isUserLoggedIn ? (
+           <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our Community!</h2>
+        ) : (
+          <h2 className='text-3xl sm:text-5xl font-semibold mb-4'> Here's What You Need To Know About Us </h2>
+        )}
+
+        {!isUserLoggedIn ? (
+            <p className='mb-8 max-w-md'>We are a diverse community of like-minded people and we welcome you to take this journey with us. We are confident that you will fit right in! Take a tour and learn more about us. </p>
+        ) : (
+            <p className='mb-8 max-w-md'>Welcome back! We're thrilled to have you as part of our community. Your presence adds value, and we can't wait to see the unique contributions you'll make. </p>
+        )}
+
+
+        {!isUserLoggedIn ? (
+                <>
+                    <p className='mb-8 max-w-md'>We are a diverse community of like-minded people and we welcome you to take this journey with us. We are confident that you will fit right in! Take a tour and learn more about us. </p>
+                    <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all cursor-pointer' onClick={()=>navigate('/login')}>Learn More</button>
+                </>
+                
           ) : (   
-             <>
-                <p className='mb-8 max-w-md'>We are a diverse community of like-minded people and we welcome you to take this journey with us. We are confident that you will fit right in! Take a tour and learn more about us. </p>
-                <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all cursor-pointer' onClick={()=>navigate('/login')}>Learn More</button>
-             </>
+
+
+
+            <>
+            <img className= "aspect-square, w-20" src={assets.hand_wave_2} />
+            
+            </>
+
+
+
+
+
         )}
     </div>
   );
