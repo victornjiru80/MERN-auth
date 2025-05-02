b510af044da87f820c5876a39b0990e73d992153
Victor <87243454+victornjiru80@users.noreply.github.com>
2025-05-02 21:25:58 +0300

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -55,9 +55,9 @@ const Login = () => {
 
       <div className='bg-white p-10 rounded-lg shadow-lg w-full sm:w-96 text-gray-800 border-grey-200 text-sm'>
 
-        <h2 className='text-3xl font-semibold text-center mb-3'>{state === 'sign up' ? 'Create Account' : 'Log in'}</h2>
+        <h2 className='text-3.5xl font-semibold text-center mb-3'>{state === 'sign up' ? 'Create Account' : 'Log in'}</h2>
 
-        <p className='text-center text-sm mb-6'>{state === 'sign up' ? 'Create your account' : 'Log in to your account'}</p>
+        <p className='text-center text-md mb-6'>{state === 'sign up' ? 'Create your account' : 'Log in to your account'}</p>
  
         <form onSubmit={onSubmitHandler} >
 
@@ -65,7 +65,7 @@ const Login = () => {
              <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full'>
               <img src={assets.person_icon} alt="" />
               <input onChange={e => setName(e.target.value)} value = {name}
-               className='bg-white outline-none rounded-md ' type="text" placeholder='Full Names' required />
+               className='bg-white outline-none rounded-md border-grey-200' type="text" placeholder='Full Names' required />
              </div>
           )}
          
@@ -73,14 +73,14 @@ const Login = () => {
               <img src={assets.mail_icon} alt="" />
               <input 
               onChange={e => setEmail(e.target.value)} value = {email}
-              className='bg-white w-full outline-none rounded-md ' type="email" placeholder='Email' required />
+              className='bg-white w-full outline-none rounded-md border-grey-200 ' type="email" placeholder='Email' required />
           </div>
 
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full'>
               <img src={assets.lock_icon} alt="" />
               <input 
                 onChange={e => setPassword(e.target.value)} value = {password}
-                className='bg-white outline-none rounded-md ' type="password" placeholder='password' required />
+                className='bg-white outline-none rounded-md border-grey-200' type="password" placeholder='password' required />
           </div>
 
               { state === 'login' && 
